87135e5c3bcd1d8b6aea94e02657f102c51c698c
Seth Fowler <mark.seth.fowler@gmail.com>
2013-12-26 17:03:48 -0800

    Store staged updates in a separate database

--- src/Pygmalion/Core.hs
+++ src/Pygmalion/Core.hs
@@ -38,6 +38,7 @@ module Pygmalion.Core
 , indexExecutable
 , pygmalionDir
 , dbFile
+, stagingDBFile
 , configFile
 , socketFile
 , compileCommandsFile
@@ -263,9 +264,10 @@ daemonExecutable = "pygd"
 indexExecutable  = "pygindex-clang"
 
 -- Data files.
-pygmalionDir, dbFile, configFile, socketFile, compileCommandsFile, tagsFile :: FilePath
+pygmalionDir, dbFile, stagingDBFile, configFile, socketFile, compileCommandsFile, tagsFile :: FilePath
 pygmalionDir        = ".pygmalion"
 dbFile              = pygmalionDir </> "index.sqlite"
+stagingDBFile       = pygmalionDir </> "staging-index.sqlite"
 configFile          = pygmalionDir </> "pygmalion.yaml"
 socketFile          = pygmalionDir </> "socket"
 compileCommandsFile = "compile_commands.json"
